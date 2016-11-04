7fc6551a47e6686e9d38fb2cfccaa679aaed97d8
Anderson Parra de Paula <anderson.parradepaula@soundcloud.com>
2016-11-04 22:21:02 +0100

    set pt_BR as default language

--- testproject/testproject/settings/base.py
+++ testproject/testproject/settings/base.py
@@ -132,7 +132,7 @@ TIME_ZONE = 'Europe/Berlin'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'pt-BR'
 
 SITE_ID = 1
 
